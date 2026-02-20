2de66c1d55bc475c646851f10a3eae18ad73bef5
al1enn <wipetmp1404@proton.me>
2026-02-20 18:34:15 +0500

    create bot

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv("secret")
+
+bot = Bot(token=str(TOKEN))
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer(f"Hi, {message.from_user.full_name}")
+
+
+@dp.message()
+async def echo_handler(message: types.Message):
+    await message.send_copy(chat_id=message.chat.id)
+    # await message.answer(text=message.text)
+
+
+async def main():
+    print("Bot is starting...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
