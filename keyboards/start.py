94ce1142656db2ad51eb72d442383d10c307b6dc
al1enn <wipetmp1404@proton.me>
2026-02-23 19:18:01 +0500

    create reply keyboard with builder

--- keyboards/start.py
+++ keyboards/start.py
@@ -1,14 +1,21 @@
-from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
+# from aiogram.types.reply_keyboard_markup import ReplyKeyboard
 from aiogram.types.keyboard_button import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 def get_start_kb():
-    button1 = KeyboardButton(text="1")
-    button2 = KeyboardButton(text="2")
-    kb = ReplyKeyboardMarkup(
-        keyboard=[
-            [button1, button1],
-            [button2],
-        ]
-    )
-    return kb
+    # button1 = KeyboardButton(text="1")
+    # button2 = KeyboardButton(text="2")
+    # kb = ReplyKeyboardMarkup(
+    #     keyboard=[
+    #         [button1, button1],
+    #         [button2],
+    #     ]
+    # )
+    # return kb
+    builder = ReplyKeyboardBuilder()
+    for index in range(1, 11):
+        builder.add(KeyboardButton(text=f"{index}"))
+        # builder.button(text=f"Set {index}")
+    builder.adjust(3)
+    return builder.as_markup()
