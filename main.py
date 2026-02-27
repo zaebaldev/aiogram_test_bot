eeda0b0e098f0bd44075c61c3fce2b1148591c47
al1enn <wipetmp1404@proton.me>
2026-02-27 18:40:26 +0500

    add logging

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from routers import router
+from aiogram.types.bot_command import BotCommand
+import logging
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+log = logging.getLogger(__name__)
+
 
 async def main():
-    print("Bot is starting...")
+    logging.basicConfig(
+        level=logging.INFO,
+    )
+    log.debug(f"token {TOKEN}")
+    cmd1 = BotCommand(
+        command="/survey",
+        description="start survey",
+    )
+    await bot.set_my_commands(
+        commands=[cmd1],
+    )
+    log.info("Bot is starting...")
     dp.include_router(router)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        log.error(e)
